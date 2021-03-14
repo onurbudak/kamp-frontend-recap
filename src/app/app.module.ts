0f1e5cfab627e1782cef3f00b0021e645ca02f83
Onur Budak <onurbudak1997@outlook.com>
2021-03-14 12:24:36 +0300

    Component ve Servisler oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarRentalComponent } from './components/car-rental/car-rental.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    CarComponent,
+    CustomerComponent,
+    CarRentalComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/car-rental/car-rental.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-rental',
+  templateUrl: './car-rental.component.html',
+  styleUrls: ['./car-rental.component.css']
+})
+export class CarRentalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
